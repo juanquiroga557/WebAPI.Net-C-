8dcac8f0098339dd400951e05d76abff8c01d747
juanquiroga557 <quirozmijito123@gmail.com>
2022-01-16 23:06:56 -0500

    Se agrego el proyecto WebAPI

---
+++ ServiciosWeb.WebAPI/Controllers/PersonaController.cs
@@ -0,0 +1,32 @@
+﻿using ServiciosWeb.Datos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiciosWeb.WebAPI.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        LibreriaConnection BD = new LibreriaConnection();
+
+        [HttpGet]
+        public IEnumerable<Persona> Get()
+        {
+            var listado = BD.Persona.ToList();
+
+            return listado;
+        }
+
+        [HttpGet]
+        public Persona GetById(int id)
+        {
+            var persona = BD.Persona.FirstOrDefault(x => x.Id == id);
+
+            return persona;
+        }
+
+    }
+}
